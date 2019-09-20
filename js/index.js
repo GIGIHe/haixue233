b1f2a90c39c291e2607c5f46ac291826f80999a7
GIGIHe <2697190181@qq.com>
2019-09-20 13:45:43 +0800

    111

---
+++ js/index.js
@@ -0,0 +1,41 @@
+$(function(){
+    $(".mfol li").click(function(){
+        $(this).addClass('hover').siblings('li').removeClass('hover');
+        $(".mfexa_type").eq($(this).index()).stop().slideDown(600).siblings(".mfexa_type").hide();
+    })
+
+    $(".point1").click(function(){
+        $("html , body").animate({scrollTop:$(".m1").offset().top},400);
+    });
+
+    $(window).scroll(function(){
+        // 注入ht
+        var top = $(".zgm1").offset().top;//获取指定位置
+        var scrollTop = $(window).scrollTop();
+        if(scrollTop>top){
+            $(".navbar").addClass('active');
+        }else{
+            $(".navbar").removeClass('active');
+        }
+    })
+
+    $(".hjul li").each(function(index){
+        $(this).click(function(){
+            $(this).addClass('hover').siblings('li').removeClass('hover');
+             $(".hjul_inner").hide().eq(index).fadeIn(200);
+        })
+    })
+    $(".tabdl dd").each(function(index){
+        $(this).click(function(){
+            $(this).addClass('hover').siblings('dd').removeClass('hover');
+             $(".table").hide().eq(index).fadeIn(200);
+        })
+    })
+    var len = $(".zgm1 .ul1 li").length
+    // console.log(len)
+    var timer = setInterval(() => {
+        var num = Math.floor(Math.random()*len)
+        $(".zgm1 .ul1 li").removeClass("hover")
+        $(".zgm1 .ul1 li").eq(num).addClass("hover")
+    }, 2500);
+})
